20004b160322f0cb7c36679f8d6fd81fa71d4932
choko0916 <n.yuka0702@gmail.com>
2022-03-20 18:00:32 +0900

    パーシャルが表示されない

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :age, numericality: true
+  validates :introduction, length: {in: 10..30}
 end
